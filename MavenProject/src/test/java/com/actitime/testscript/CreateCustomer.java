576adf82f3f1e12501312df26bbb1f6ae63fcc59
DELL <DELL@DESKTOP-40TAOVD>
2024-01-05 13:18:19 +0530

    first commit

---
+++ MavenProject/src/test/java/com/actitime/testscript/CreateCustomer.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+ @Test
+ public void createCustomer(){
+	 Reporter.log("Creating Customer",true);
+ }
+ 
+}
